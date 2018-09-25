a18c1c2d0a3f9b1d82dbfad7332ccf9c3abda8dc
tam <Tam.McLaughlin@collectionhq.com>
2018-09-25 16:20:50 +0100

    updated msg

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello Earth\n')
+   response.end('Hello Mars\n')
 }).listen(3000)
 
 // Console will print the message
